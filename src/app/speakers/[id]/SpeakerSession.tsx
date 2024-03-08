9bfdc39a1e123f529e8d805a99c82440528ec03c
Jalil Arfaoui <jalil@arfaoui.net>
2024-03-08 17:29:35 +0100

    feat: add a heart for plan/unplan feature

--- src/app/speakers/[id]/SpeakerSession.tsx
+++ src/app/speakers/[id]/SpeakerSession.tsx
@@ -3,13 +3,20 @@ import {useSessionStore} from "@/state/state";
 import Link from "next/link";
 import {langCodeToFlag} from "@/app/speakers/[id]/langCodeToFlag";
 import {SessionDisplayed} from "@/app/speakers/[id]/page";
+import {Button} from "@/components/MovingBorder";
 
-export const SpeakerSession = ({sessions}: { sessions: SessionDisplayed[]}) => {
+
+export const SpeakerSession = ({sessions}: { sessions: SessionDisplayed[] }) => {
     const store = useSessionStore()
 
     return sessions.map(({session, period, room}) => {
         const planned = store.plannedSessions.includes(session.id)
 
+        const heartIcon = <svg xmlns="http://www.w3.org/2000/svg" fill={planned ? "red" : "none"} viewBox="0 0 24 24" strokeWidth={1.5}
+                               stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
+        </svg>;
         if (session.type === "Talk") {
             return (
                 <Link href={`/schedule/${session.id}`} key={session.id}>
@@ -21,12 +28,32 @@ export const SpeakerSession = ({sessions}: { sessions: SessionDisplayed[]}) => {
                                 </div>
                                 <div>{session.title}</div>
                             </div>
-                            <div className="text-sm text-gray-600">
-                                {period.start_date.toLocaleString()}
+                            <div className="flex w-full content-between">
+                                <div className="text-sm text-gray-600">
+                                    {period.start_date.toLocaleString()}
+                                </div>
+                                <Button
+                                    borderRadius="1.75rem"
+                                    className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                                >
+                                    {
+                                        planned
+                                            ? <button onClick={(e) => {
+                                                e.preventDefault()
+                                                store.unplanSession(session.id);
+                                            }}>{heartIcon}</button>
+
+                                            : <button onClick={(e) => {
+                                                e.preventDefault()
+                                                store.planSession(session.id);
+                                            }}>{heartIcon}</button>
+                                    }
+                                </Button>
+
                             </div>
                             <div className="text-sm text-gray-600">{room.name}</div>
                             <div className="flex pt-4  space-x-4  ">
-                                {session.tags?.map((tag) => (
+                            {session.tags?.map((tag) => (
                                     <div
                                         key={tag.id}
                                         className="cursor-pointer text-gray-600 rounded-xl bg-red-50 w-1/5  border text-sm border-red-600 text-center "
@@ -35,17 +62,8 @@ export const SpeakerSession = ({sessions}: { sessions: SessionDisplayed[]}) => {
                                     </div>
                                 ))}
                             </div>
-                            {
-                                planned
-                                    ? <>Planned !  <button onClick={(e) => {
-                                        e.preventDefault()
-                                        store.unplanSession(session.id);
-                                    }}>Unplan</button></>
-                                    :   <button onClick={(e) => {
-                                        e.preventDefault()
-                                        store.planSession(session.id);
-                                    }}>Plan</button>
-                            }
+
+
                         </div>
                     </div>
                 </Link>
@@ -53,3 +71,6 @@ export const SpeakerSession = ({sessions}: { sessions: SessionDisplayed[]}) => {
         }
     });
 };
+
+
+
