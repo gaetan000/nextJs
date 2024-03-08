390497fd600418c1edb018dde951c358cfcb3523
Gaëtan Michel <gaetanmichel@MacBook-Pro-de-Gaetan.local>
2024-03-08 15:10:18 +0100

    feat(Speaker/id): add avatar + speaker description + shadcn

--- src/app/speakers/[id]/page.tsx
+++ src/app/speakers/[id]/page.tsx
@@ -1,31 +1,58 @@
-import { Speaker} from "@/types/Speaker"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Speaker, Speaker } from "@/types/Speaker";
+import Image from "next/image";
 
 // export const dynamicParams = false;
 // export const dynamic = "force-static";
 
 export async function generateStaticParams() {
- 
-  
-    const speakers = await fetch('https://konfetti.monkeypatch.io/web/conferences/demo-konfetti-2023/speakers').then((res) => res.json())
+  const speakers = await fetch(
+    "https://konfetti.monkeypatch.io/web/conferences/demo-konfetti-2023/speakers"
+  ).then((res) => res.json());
 
-    return speakers.map((speaker: Speaker) => ({
-      id: `${speaker.id}`,
-    }));
+  return speakers.map((speaker: Speaker) => ({
+    id: `${speaker.id}`,
+  }));
+}
+
+export default async function SpeakerPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  if (!id) {
+    throw new Error("Speaker not found");
   }
-export default async function Speaker({ params }: { params: { id: string } }) {
-    const { id } = params;
-    if(!id) {
-      throw new Error("Speaker not found");
-    }
-    const speaker = await fetch(`https://konfetti.monkeypatch.io/web/conferences/demo-konfetti-2023/speakers/${id}`).then((res) => res.json())
-    if(!speaker) {
-      throw new Error("Speaker not found");
-    }
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-start p-24">
-        <h1>Speaker n° {speaker.id}</h1>
-        <h2>{speaker.name}</h2>
-        <p>{speaker.bio}</p>
-      </main>
-    );
+  const speaker: Speaker = await fetch(
+    `https://konfetti.monkeypatch.io/web/conferences/demo-konfetti-2023/speakers/${id}`
+  ).then((res) => res.json());
+  if (!speaker) {
+    throw new Error("Speaker not found");
+  }
+
+  const avatarFallaback = speaker.full_name
+    .split(" ")
+    .map((name) => name[0])
+    .join("");
+  return (
+    <main className="flex min-h-screen flex-col items-start justify-start p-24">
+      <div className="text-2xl">Speakers</div>
+      <div className="flex items-center justify-start space-x-5 pt-8">
+        <Avatar>
+          <AvatarImage
+            src={speaker.avatar_url}
+            alt={`Image of ${speaker.full_name}`}
+          />
+          <AvatarFallback>{avatarFallaback}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <div>{speaker.full_name}</div>
+          <div className="text-gray-600">{speaker.company}</div>
+        </div>
+      </div>
+      <div className="w-48 h-48 rounded-full"></div>
+      <p>{speaker.bio}</p>
+    </main>
+  );
 }
