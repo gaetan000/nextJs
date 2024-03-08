8f2a0b7770712d51e4c3e32a65b0026edb9bb58b
David <david-bourrel@hotmail.fr>
2024-03-08 16:36:07 +0100

    logo added

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image";
+import logo from "../../assets/logo-monochrome.svg";
 
 export default function HeaderLogo() {
   return (
     <div className="text-slate-500 flex items-center">
-      {/* <Image
-        src={"https://hoverboard-master.web.app/"}
-        height={32}
-        width={150}
-        alt=""
-      /> */}
-      LOGO
+      <Image src={logo} height={32} width={150} alt="" />
     </div>
   );
 }
